0c76f15c17f6ebd938eeafb5170d79e114ace7f0
Your Name Here <youremailhere>
2020-04-07 23:24:22 -0400

    Finished project

--- RainBow (iOSPT6)/View Controllers/ColorsTableViewController.swift
+++ RainBow (iOSPT6)/View Controllers/ColorsTableViewController.swift
@@ -9,82 +9,54 @@
 import UIKit
 
 class ColorsTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+    
+    // red, org, yello, green, blue, indigo, purple 
+    var colors: [Color] = [
+        Color(colorName: "Red", color: .red),
+        Color(colorName: "Blue", color: .blue),
+        Color(colorName: "Yellow", color: .yellow)
+    ]
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    // This is Requried -- How many rows should the table have?
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return colors.count
     }
 
-    /*
+    // This is also Required -- What data should each row contain?
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell", for: indexPath)
 
-        // Configure the cell...
-
+        // Index Path (Section, Row)
+        // (0,0)
+        // (0,1)
+        // 1. get the current color for the row of colors (Subscrpit Syntax)
+        let color = colors[indexPath.row]
+        cell.textLabel?.text = color.colorName
+        cell.backgroundColor = color.color
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        
+        if segue.identifier == "ToDetailSegue" {
+            if let detailVC = segue.destination as? ColorDetailViewController,
+                let indexPath = self.tableView.indexPathForSelectedRow {
+                detailVC.cellColor = colors[indexPath.row]
+            }
+        }
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
